6422f5deb90e5685aa38c289fcf284f77b7b868e
Sumit Kumar Sourabh <sumitsourabh1993@gmail.com>
2024-10-07 12:55:47 +0530

    fixed lanting error

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 def add(a, b):
-    return a+b
+    return a + b
 
 
 def sub(a, b):
-    return a-b
+    return a - b
 
 
 def mul(a, b):
-    return a*b
+    return a * b
 
 
-if __name__=='__main__': 
+if __name__ == '__main__':
     result1 = add(4, 5)
     result2 = sub(4, 3)
     print(result1, result2)
